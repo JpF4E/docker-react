38546dd8577b12bf8026a4cc715fca39c115424f
joão Rodrigues <joaoprodrigues@ua.pt>
2021-03-22 00:41:03 +0000

    added travis file 6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 	<p>
-	  Shut the fuck up, please! llol
+	  Shut the fuck up, please! llol THIS IS A FEATURE
 	</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
